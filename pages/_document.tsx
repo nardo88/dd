533373a3f8d980ed18d78f670c2d6c26b71ecca4
nardo88 <nardo1988@mail.ru>
2024-04-14 09:50:32 +0300

    feat: добавил favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,700&display=swap"
           rel="stylesheet"
         />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="favicons/favicon-96x96.png"
+        />
       </Head>
       <body>
         <Main />
