b4c5b30717c964d56ec80aeb46db9f975f34caa9
nardo88 <nardo1988@mail.ru>
2025-02-15 17:14:38 +0300

    remove loader

--- shared/ui/MarkDownEditor/ui/MarkDownEditor.async.tsx
+++ shared/ui/MarkDownEditor/ui/MarkDownEditor.async.tsx
@@ -1,10 +1,6 @@
-import { Loader } from '@shared/ui/Loader/Loader'
 import dynamic from 'next/dynamic'
 
 export const AsyncMarkDownEditor = dynamic(
   () => import('./MarkDownEditor').then((mode) => mode.MarkDownEditor),
-  {
-    loading: () => <Loader />,
-    ssr: false,
-  }
+  { ssr: false }
 )
--- shared/ui/MarkdownViewer/ui/MarkdownViewer.async.tsx
+++ shared/ui/MarkdownViewer/ui/MarkdownViewer.async.tsx
@@ -1,11 +1,6 @@
 import dynamic from 'next/dynamic'
 
-import { Loader } from '@shared/ui/Loader/Loader'
-
 export const AsyncMarkdownViewer = dynamic(
   () => import('./MarkdownViewer').then((mode) => mode.MarkdownViewer),
-  {
-    loading: () => <Loader />,
-    ssr: false,
-  }
+  { ssr: false }
 )
